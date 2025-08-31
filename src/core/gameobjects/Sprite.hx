2a9b181a240ffac919bf5016c984246d0fcb0615
zzox <tweedlitweedli@gmail.com>
2025-08-30 21:59:07 -0500

    remove commented out code for scaling and rotating sprites

--- src/core/gameobjects/Sprite.hx
+++ src/core/gameobjects/Sprite.hx
@@ -37,30 +37,10 @@ class Sprite extends GameObject {
     override function update (delta:Float) {}
 
     override function render (g2:Graphics, camera:Camera) {
-        // load placeholder? use asset filter in the beginning?
-
-        // g2.color = Math.floor(alpha * 256) * 0x1000000 + color;
-
-        // rotate, translate, then scale
-        // g2.pushRotation(toRadians(angle), getMidpoint().x, getMidpoint().y);
-        // g2.pushTranslation(-camera.scroll.x * scrollFactor.x, -camera.scroll.y * scrollFactor.y);
-        // g2.pushScale(camera.scale.x, camera.scale.y);
-
         g2.pushTranslation(-camera.scrollX * scrollFactorX, -camera.scrollY * scrollFactorY);
 
         // draw a cutout of the spritesheet based on the tileindex
         final cols = Std.int(image.width / sizeX);
-        // g2.drawScaledSubImage(
-        //     image,
-        //     (tileIndex % cols) * size.x,
-        //     Math.floor(tileIndex / cols) * size.y,
-        //     size.x,
-        //     size.y,
-        //     x + ((size.x - size.x * scale.x) / 2) + (flipX ? size.x * scale.x : 0),
-        //     y + ((size.y - size.y * scale.y) / 2) + (flipY ? size.y * scale.y : 0),
-        //     size.x * scale.x * (flipX ? -1 : 1),
-        //     size.y * scale.y * (flipY ? -1 : 1)
-        // );
         g2.drawScaledSubImage(
             image,
             (tileIndex % cols) * sizeX,
@@ -73,8 +53,6 @@ class Sprite extends GameObject {
             sizeY * (flipY ? -1 : 1)
         );
 
-        // g2.popTransformation();
         g2.popTransformation();
-        // g2.popTransformation();
     }
 }
